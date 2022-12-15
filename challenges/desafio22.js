a1eed53b4c5b8d540cea7fc22985b030a81d7c26
Diego-Sabino <dbrgamei5@gmail.com>
2022-12-15 16:46:56 -0300

    fix eslint

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -2,9 +2,9 @@ db.produtos.updateMany({},
   { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] },
 });
 
-db.produtos.updateOne({ nome: "Big Mac"}, { $inc: { "vendasPorDia.3": 60 } } );
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
-db.produtos.updateOne({ tags: "bovino" }, { $inc: { "vendasPorDia.6": 120 } } );
+db.produtos.updateOne({ tags: "bovino" }, { $inc: { "vendasPorDia.6": 120 } });
 
 db.produtos.find({}, {
   vendasPorDia: true,
